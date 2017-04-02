3a9b8913486a23f842cc3421c3a753a4628d1402
Unknown <jubleshum@gmail.com>
2017-04-01 22:25:28 -0700

    Added MapDrawingView class

---
+++ Project6/app/src/main/java/com/cs3680/justin/project6/MapDrawingView.java
@@ -0,0 +1,41 @@
+package com.cs3680.justin.project6;
+
+/**
+ * Created by justi on 3/14/2017.
+ */
+
+public class MapDrawingView {
+}
+
+
+/*
+* Have two FOR statements that reads the two dimensional array.
+
+for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < array[].length; i++) {
+
+
+    }
+}
+
+http://stackoverflow.com/questions/5627580/2d-array-in-android
+
+
+
+for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < array[].length; i++) {
+        //DRAW BITMAP AT x,y
+    }
+}
+
+
+for (int srow = 0; i < array.length; i++) {
+    for (int scol = 0; i < array[].length; i++) {
+        wrow, wcol = screenToWorldap(wrow, wcol)
+        bitmap = worldMap(wrow, wcol)
+        y = srow * 60
+        x = scol * 60
+        drwaBitmap(bitmap, x, y, paint
+    }
+}
+*/
